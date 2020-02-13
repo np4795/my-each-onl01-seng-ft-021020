3307cd9143906a0ea99533b6f462820ee425435c
Nicholas Patel <np4795@yahoo.com>
2020-02-13 23:44:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,23 @@
-def my_each # put argument(s) here
-  # code here
+list = ["ab", "cd", "ef"]
+
+def my_each(array)
+  i = 0
+  while i < array.length
+# The "while" method will always return a value of => nil"
+    yield(array[i])
+    i = i + 1
+  end
 end
+
+
+
+def hello(tas)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
